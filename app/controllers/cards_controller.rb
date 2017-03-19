c8d7ec6b2ef6758d09e3fe7b930b237ab709fe54
Charlie <charliecuddy4@gmail.com>
2017-03-19 13:43:43 -0500

    add simple_form, bootstrap, haml gems and Render forms with New

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,46 @@
+class CardsController < ApplicationController
+   before_action :find_card, only: [:show, :edit, :update, :destroy]
+
+  def index
+  	@cards = Card.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+  	@card = Card.new
+  end
+
+  def create
+  	@card = Card.new(card_params)
+
+  	if @card.save
+  	  redirect_to @card, notice: "Card Created"
+  	else
+  	  render 'new'
+  	end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  	@card.destroy
+    redirect_to root_path
+  end
+
+
+  private
+
+  def card_params
+    params.require(:card).permit(:title, :description)
+  end
+
+  def find_card
+  	@card = Card.find(params[:id])
+  end
+end
