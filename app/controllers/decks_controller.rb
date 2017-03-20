e2e074f609153b9e78ae0d6d19bb6ef129493691
Charlie <charliecuddy4@gmail.com>
2017-03-19 21:43:47 -0500

    Create Deck model and Decks control and associations

---
+++ app/controllers/decks_controller.rb
@@ -0,0 +1,54 @@
+class DecksController < ApplicationController
+
+   before_action :find_deck, only: [:show, :edit, :update, :destroy]
+
+  def index
+  	@decks = Deck.all.order("created_at DESC")
+  end
+
+  def show
+
+  end
+
+  def new
+  	@deck = Deck.new
+  end
+
+  def create
+  	@deck = Deck.new(card_params)
+
+  	if @deck.save
+  	  redirect_to @deck, notice: "Deck Created"
+  	else
+  	  render 'new'
+  	end
+  end
+
+  def edit
+  end
+
+  def update
+  	if @deck.update(deck_params)
+  	  redirect_to @deck, notice: "Deck Updated"
+  	else
+  	  render 'edit'
+  	end
+  end
+
+  def destroy
+  	@deck.destroy
+    redirect_to root_path
+  end
+
+
+  private
+
+  def deck_params
+    params.require(:deck).permit(:title, :description)
+  end
+
+  def find_deck
+  	@deck = Deck.find(params[:id])
+  end
+end
+end
